61cb3b59de9645a2fac20905290f918cdb2cc2cd
rayhankabir-me <devrayhankabir@gmail.com>
2024-07-30 18:21:08 +0600

    react router dom, react firebase hook

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU53yPdgAkJfwMtYoTx2OUADKdVJv6ItE",
+  authDomain: "guestbook-5ecbd.firebaseapp.com",
+  projectId: "guestbook-5ecbd",
+  storageBucket: "guestbook-5ecbd.appspot.com",
+  messagingSenderId: "575225219296",
+  appId: "1:575225219296:web:3f3cd244fd5406983d2885",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
