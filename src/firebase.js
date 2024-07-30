6e604a79bf4aebbd11d1e76f4da302bdc2cb8856
rayhankabir-me <devrayhankabir@gmail.com>
2024-07-30 20:22:41 +0600

    registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const registerWithEmailAndPassword = async (email, password) => {
+  try {
+    const respond = createUserWithEmailAndPassword(auth, email, password);
+    const user = (await respond).user;
+    console.log(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { registerWithEmailAndPassword };
--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerWithEmailAndPassword } from "../firebase";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await registerWithEmailAndPassword(email, password);
+      console.log(user);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <h1>Register Page</h1>
+      <div>
+        <h1>Register Page</h1>
+        <form action="#">
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="=password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button onClick={handleSubmit}>Register</button>
+        </form>
+      </div>
     </>
   );
 }
